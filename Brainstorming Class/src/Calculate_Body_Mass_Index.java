5475e170d4147229e401cb9b01203d20b1e5cde3
Rishab Dussoye <Rishab Dussoye@DESKTOP-LGK7UGC>
2019-07-19 01:38:56 +0400

    First Comit

---
+++ Brainstorming Class/src/Calculate_Body_Mass_Index.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Rishab Dussoye
+ *
+ */
+public class Calculate_Body_Mass_Index {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
